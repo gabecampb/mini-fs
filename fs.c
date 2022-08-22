87a3af090ae58ed1fa4671a2c3e8075c2a32ca35
Gabriel Campbell <gabecampb@users.noreply.github.com>
2022-08-22 13:13:19 -0300

    Added inode deletion

--- fs.c
+++ fs.c
@@ -24,7 +24,10 @@ void init_disk() {
 // node data (file contents)
 
 uint32_t n_max_inodes = 512;
+uint8_t* used_inode_states;		// for each of n_used_inodes, 0 for existing inodes and 1 for deleted inodes
 uint32_t n_used_inodes;
+uint32_t* deleted_inodes;		// list of all currently deleted (unused) inodes
+uint32_t n_deleted_inodes;
 uint32_t inode_width = 9;
 
 uint32_t metadata_width = 33;	// 32 bytes for filename, 1 byte for node type (directory/file)
@@ -36,9 +39,45 @@ uint32_t n_used_regions;
 
 
 // add a new inode to the inode table and return its ID, or -1 on fail
+// reuse a deleted inode if applicable
 int32_t new_inode() {
+	if(n_deleted_inodes > 1) {
+		int32_t inode = deleted_inodes[n_deleted_inodes-1];
+		deleted_inodes = realloc(deleted_inodes, sizeof(uint32_t) * (n_deleted_inodes - 1));
+		used_inode_states[inode] = 0;	// restored inode; mark as existing
+		n_deleted_inodes--;
+		return inode;
+	} else if(n_deleted_inodes == 1) {
+		int32_t inode = deleted_inodes[0];
+		free(deleted_inodes);
+		deleted_inodes = 0;
+		used_inode_states[inode] = 0;	// restored inode; mark as existing
+		n_deleted_inodes--;
+		return inode;
+	}
+
 	if(n_used_inodes + 1 > n_max_inodes) return -1;
+	used_inode_states = realloc(used_inode_states, sizeof(uint8_t) * (n_used_inodes + 1));
+	used_inode_states[n_used_inodes] = 0;	// mark inode as existing
 	return n_used_inodes++;
+}
+
+// mark an inode in the inode table as deleted, and remove associated node from used_regions
+void delete_inode(uint32_t inode) {
+	if(inode >= n_used_inodes) return;
+	for(uint32_t i = 0; i < n_used_regions; i++)
+		if(used_regions[i] == *(uint64_t*)&disk[inode * inode_width + 1]) {
+			// remove used_regions[i] and used_region_lengths[i]
+			used_regions[i] = used_regions[n_used_regions - 1];
+			used_region_lengths[i] = used_region_lengths[n_used_regions - 1];
+			used_regions = realloc(used_regions, sizeof(uint64_t) * (n_used_regions - 1));
+			used_region_lengths = realloc(used_region_lengths, sizeof(uint64_t) * (n_used_regions - 1));
+			n_used_regions--;
+		}
+	used_inode_states[inode] = 1;	// mark inode as deleted
+	deleted_inodes = realloc(deleted_inodes, sizeof(uint32_t) * (n_deleted_inodes + 1));
+	deleted_inodes[n_deleted_inodes] = inode;
+	n_deleted_inodes++;
 }
 
 // name should be null-terminated and with a max length of 32.
@@ -72,8 +111,8 @@ int32_t new_node(uint8_t* name, uint32_t n_bytes_data, uint32_t parent_inode, ui
 	*(uint64_t*)(iptr + 1) = min_address;
 
 	// add min_address, max_address to used_regions.
-	used_regions = realloc(used_regions, sizeof(uint64_t) * (n_used_regions+1));
-	used_region_lengths = realloc(used_region_lengths, sizeof(uint64_t) * (n_used_regions+1));
+	used_regions = realloc(used_regions, sizeof(uint64_t) * (n_used_regions + 1));
+	used_region_lengths = realloc(used_region_lengths, sizeof(uint64_t) * (n_used_regions + 1));
 	used_regions[n_used_regions] = min_address;
 	used_region_lengths[n_used_regions] = metadata_width + n_bytes_data;
 	n_used_regions++;
